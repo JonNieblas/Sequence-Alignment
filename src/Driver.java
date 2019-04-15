d88956122e7949741e0750ca04f2c32da96aae97
Jonathan Nieblas <jniebla1@msudenver.edu>
2019-04-15 17:49:25 -0600

    Added print statements describing how to build the two strings from the dynamic table.

--- src/Driver.java
+++ src/Driver.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
 public class Driver {
     public static void main(String[] args){
+
         Scanner s = new Scanner(System.in);
 
-//        System.out.println("Please input a string for x: ");
-//        String x = s.next();
-//
-//        System.out.println("Please input a string for y: ");
-//        String y = s.next();
-        String y = "TAAGGTCA";
-        String x = "AACAGTTACC";
+        System.out.println("Please input a string for x: ");
+        String x = s.next();
 
-        String y1 = "GACATATTAC";
-        String x1 = "AACGTAGAC";
+        System.out.println("Please input a string for y: ");
+        String y = s.next();
+
+//        String y = "TAAGGTCA";
+//        String x = "AACAGTTACC";
+//
+//        String y1 = "GACATATTAC";
+//        String x1 = "AACGTAGAC";
+
         Sequence s1 = new Sequence();
 
         s1.startSequence(x, y);
-        s1.startSequence(x1, y1);
+        //s1.startSequence(x1, y1);
     }
 }
--- src/Sequence.java
+++ src/Sequence.java
@@ -7,14 +7,17 @@
  *      For char i of string x and char j of string y:
  *          If i matches j, reward = 0 points.
  *          If i doesn't match j, reward = 1 point.
- *          If i is paired with a space, reward = 2 points.
- *          If j is paired with a space, reward = 2 points.
+ *          If i is paired with a SPACE, reward = 2 points.
+ *          If j is paired with a SPACE, reward = 2 points.
  * The system finds the optimal way to input spaces into both
  * strings by computing the score with the lowest amount of points,
  * then tracing backwards to build the two strings.
  */
 public class Sequence {
-    private static final int space = 2;
+    private static final int SPACE = 2;
+    private static final char XCHAR = 'x';
+    private static final char YCHAR = 'y';
+    private static final char NONECHAR = 'n';
 
     // holds our best XY vals for testing
     private static String[] bestXY = new String[2];
@@ -23,33 +26,57 @@ public class Sequence {
     }
 
     // Takes the table and builds our best strings.
-    private static void buildAnswer(String x, String y, int m, int n, Score[][] alignedTable){
+    private static void buildAnswer(String x, String y, int m,
+                                    int n, Score[][] alignedTable){
         int i = m;
         int j = n;
         int xIndex = m - 1;
         int yIndex = n - 1;
         String xBest = "";
         String yBest = "";
+        char sp = '_';
 
+        System.out.println("\nSteps to build our optimal x and y:");
         while(i != 0 || j != 0){
-            char choice = alignedTable[i][j].getChoice();
-            if(choice == 'n'){
-                xBest = x.charAt(xIndex) + xBest;
-                yBest = y.charAt(yIndex) + yBest;
+            Score s = alignedTable[i][j];
+            char choice = s.getChoice();
+            int points = s.getPoints();
+            System.out.println(String.format(
+                    "\nNext Score: %d%c Point: (%d, %d)",
+                    points, choice, i, j));
+
+            char xChar = x.charAt(xIndex);
+            char yChar = y.charAt(yIndex);
+
+            if(choice == NONECHAR){
+                System.out.println(String.format(
+                        "Adding: %c to x string: %s", xChar, xBest));
+                System.out.println(String.format(
+                        "Adding: %c to y string: %s", yChar, yBest));
+                xBest = xChar + xBest;
+                yBest = yChar + yBest;
                 xIndex--;
                 yIndex--;
                 i--;
                 j--;
             }
-            else if(choice == 'x'){
-                yBest = "_" + yBest;
-                xBest = x.charAt(xIndex) + xBest;
+            else if(choice == XCHAR){
+                System.out.println(String.format(
+                        "Adding: %c to x string: %s", xChar, xBest));
+                System.out.println(String.format(
+                        "Adding: %c to y string: %s", sp, yBest));
+                yBest = sp + yBest;
+                xBest = xChar + xBest;
                 xIndex--;
                 i--;
             }
             else{
-                xBest = "_" + xBest;
-                yBest = y.charAt(yIndex) + yBest;
+                System.out.println(String.format(
+                        "Adding: %c to x string: %s", sp, xBest));
+                System.out.println(String.format(
+                        "Adding: %c to y string: %s", yChar, yBest));
+                xBest = sp + xBest;
+                yBest = yChar + yBest;
                 yIndex--;
                 j--;
             }
@@ -63,10 +90,10 @@ public class Sequence {
         Score[][] alignedTable = new Score[m + 1][n + 1];
 
         for(int i = 0; i <= m; i++){
-            alignedTable[i][0] = new Score(space * i, 'n');
+            alignedTable[i][0] = new Score(SPACE * i, NONECHAR);
         }
         for(int j = 0; j <= n; j++){
-            alignedTable[0][j] = new Score(space * j, 'n');
+            alignedTable[0][j] = new Score(SPACE * j, NONECHAR);
         }
 
         return alignedTable;
@@ -91,36 +118,42 @@ public class Sequence {
             char yChar = y.charAt(j - 1);
             for(int i = 1; i <= m; i++){
                 char xChar = x.charAt(i - 1);
-                int none = alignedTable[i - 1][j - 1].getPoints() + getPairValue(xChar, yChar);
-                int xDash = alignedTable[i - 1][j].getPoints() + space;
-                int yDash = alignedTable[i][j - 1].getPoints() + space;
+                int none = alignedTable[i - 1][j - 1].
+                        getPoints() + getPairValue(xChar, yChar);
+                int xDash = alignedTable[i - 1][j].getPoints() + SPACE;
+                int yDash = alignedTable[i][j - 1].getPoints() + SPACE;
 
                 if(none <= xDash && none <= yDash){
-                    alignedTable[i][j] = new Score(none, 'n');
+                    alignedTable[i][j] = new Score(none, NONECHAR);
                 }
                 else if(xDash <= yDash){
-                    alignedTable[i][j] = new Score(xDash, 'x');
+                    alignedTable[i][j] = new Score(xDash, XCHAR);
                 }
                 else{
-                    alignedTable[i][j] = new Score(yDash, 'y');
+                    alignedTable[i][j] = new Score(yDash, YCHAR);
                 }
             }
         }
 
-        buildAnswer(x, y, m, n, alignedTable);
         printTable(x, y, m, n, alignedTable);
+        buildAnswer(x, y, m, n, alignedTable);
         printBestStrings();
     }
 
     // Prints table containing all Scores
-    private static void printTable(String x, String y, int m, int n, Score[][] alignedTable){
+    private static void printTable(String x, String y, int m, int n,
+                                   Score[][] alignedTable){
         int yIndex = 0;
 
         // Print table info:
-        System.out.println("\nTable of each score's total points and optimal choice.");
-        System.out.println(String.format("%30s", "x = place dash in string x"));
-        System.out.println(String.format("%30s", "y = place dash in string y"));
-        System.out.println(String.format("%44s", "n = don't place a dash in either string\n"));
+        System.out.println("\nTable of each score's " +
+                "total points and optimal choice.");
+        System.out.println(String.format("%30s",
+                "x = place dash in string x"));
+        System.out.println(String.format("%30s",
+                "y = place dash in string y"));
+        System.out.println(String.format("%44s",
+                "n = don't place a dash in either string\n"));
 
         // Print top x-axis
         System.out.print(String.format("%4c %3c   ", '|', '-'));
@@ -134,7 +167,8 @@ public class Sequence {
             if(j == 0){ // Print initial dash
                 System.out.print(String.format("\n%-2c ", '-'));
             } else { // Print y-axis value
-                System.out.print(String.format("\n%-2c ", y.charAt(yIndex)));
+                System.out.print(String.format("\n%-2c ",
+                        y.charAt(yIndex)));
                 yIndex++;
             }
 
@@ -146,6 +180,7 @@ public class Sequence {
             }
             printRowLines(m);
         }
+        System.out.println(" ");
     }
 
     // print the bottoms lines of the table's rows
@@ -159,7 +194,7 @@ public class Sequence {
 
     // Prints the best string for x and y
     private static void printBestStrings(){
-        System.out.println("\n\nFinal x: " + bestXY[0]);
+        System.out.println("\nFinal x: " + bestXY[0]);
         System.out.println("Final y: " + bestXY[1]);
     }
 
